2fc41da30870f47a4b0b94cc8a5c1c9063e24e63
Hannah Swift <h.graceswift@gmail.com>
2016-07-22 15:08:30 -0500

    Custom paths for run notes, dynamic IDs

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('new', { path: 'new'}); // New Run
-  this.route('run-detail', { path: 'run-detail' });
+  this.route('run-detail', { path: '/:detail' });
 });
 
 export default Router;
--- app/routes/run-detail.js
+++ app/routes/run-detail.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model({ _id }) {
+  model(_id ) {
+    console.log(_id);
     // const id = params.id;
     // const { id } = params;
 
-    return fetch (`http://tiny-tn.herokuapp.com/collections/runs-hs${_id}`)
+    return fetch (`http://tiny-tn.herokuapp.com/collections/runs-hs/${_id.detail}`)
       .then((res) => res.json());
   }
 });
